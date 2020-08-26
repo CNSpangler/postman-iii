8d369d8074d4defde9d77258785202142246d74d
Chelsea Spangler <chelseanspangler@gmail.com>
2020-08-25 17:24:41 -0700

    radio buttons generate dynamically

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,25 +2,22 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styles from './App/App.css';
 
-const url = useSelector(getUrl);
-
+// const url = useSelector(getUrl);
 const methods = ['POST', 'GET', 'PATCH', 'PUT', 'DELETE'];
+console.log(methods.map(method => method));
 
 const RadioButtons = methods.map(method => (
-  <>
+  <div className={styles.radioButtonContainer} key={method}>
     <input id={method} type="radio" name={method} value={method} />
     <label className={styles.radioButton} htmlFor={method}>{method}</label>
-  </>
+  </div>
   )
 );
 
 const Form = () => (
   <div className={styles.Form}>
-    <div className={styles.radioButtonContainer}>
-    <input id="POST" type="radio" name="POST" value="POST" />
-    <label className={styles.radioButton} htmlFor="POST">"POST"</label>
-
-      {/* {radioButtons} */}
+    <div className={styles.RadioGroup}>
+      {RadioButtons}
     </div>
 
     <input type="text" className={styles.url} />
