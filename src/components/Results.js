a6811e4999f849e9606d52b2ec61995a83dc5ea9
Chelsea Spangler <chelseanspangler@gmail.com>
2020-08-26 14:58:25 -0700

    Results panel renders results from state

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactJson from 'react-json-view';
 import styles from './App/App.css';
+import { useSelector } from 'react-redux';
+import { getResults } from '../redux/selectors';
 
 const Results = () => {
+  const results = useSelector(getResults);
 
   return (
     <div className={styles.Results}>
-      {/* {results ? <ReactJson src={results} /> : <div>Please make a request above</div>} */}
+      {results ? <ReactJson src={results} /> : <div>Please make a request above</div>}
       <h1>RESULTS</h1>
     </div>
   );
