a3b0bf751e18c9efd3858172cc886dc3f94a11ff
Chelsea Spangler <chelseanspangler@gmail.com>
2020-08-27 17:16:24 -0700

    update API call to incorporate body and header for relevant methods

--- services.js
+++ services.js
@@ -1,5 +1,11 @@
-export const fetchData = (url) => {
-  return fetch(url)
+export const fetchData = (url, method, body) => {
+  return fetch(url, {
+    method,
+    body: (method === 'GET' || method === 'DELETE') ? null : JSON.stringify(JSON.parse(body)),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  })
     .then(res => res.json())
     .catch(err => console.log(err));
 }
